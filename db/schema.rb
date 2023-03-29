8ec08c11de0a6d5b4c2cf2e11b3af4f291d37cba
Carlos Pena Caballero <carlos.penacaballero01@utrgv.edu>
2023-03-29 14:10:04 +0000

    Upgraded framework

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2020_04_28_170822) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_140710) do
   create_table "grades", force: :cascade do |t|
     t.string "student_id"
     t.string "student_name"
