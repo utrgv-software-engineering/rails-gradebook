2f5c6ef522381f146ab470b583f6fb4657597a83
Carlos Pena Caballero <carlos.penacaballero01@utrgv.edu>
2023-03-30 17:29:23 +0000

    yarn build works

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -10,7 +10,7 @@ import"./channels"
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
