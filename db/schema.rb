e2b851f7fe22315ca74acd36a6e0244afe673c31
Carlos Pena Caballero <carlos.penacaballero01@utrgv.edu>
2024-10-22 15:26:21 +0000

    Works!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_140710) do
+ActiveRecord::Schema[7.2].define(version: 2020_04_28_170822) do
   create_table "grades", force: :cascade do |t|
     t.string "student_id"
     t.string "student_name"
@@ -30,5 +30,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_140710) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
